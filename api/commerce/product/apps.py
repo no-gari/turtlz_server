55f81f891b97b02b4cf238d003c267c8f4435dd3
no-gari <sofaissofa@icloud.com>
2022-03-28 11:27:47 +0900

    added modules

--- api/commerce/product/apps.py
+++ api/commerce/product/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommerceConfig(AppConfig):
+class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api.commerce'
+    name = 'api.commerce.product'
---
+++ api/commerce/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('cart/', include('api.commerce.cart.urls')),
+    path('order/', include('api.commerce.order.urls')),
+    path('brand/', include('api.commerce.brand.urls')),
+    path('review/', include('api.commerce.review.urls')),
+    path('coupon/', include('api.commerce.coupon.urls')),
+    path('payment/', include('api.commerce.payment.urls')),
+    path('product/', include('api.commerce.product.urls')),
+    path('invoice/', include('api.commerce.invoice.urls')),
+    path('address/', include('api.commerce.address.urls')),
+    path('category/', include('api.commerce.category.urls')),
+    path('checkout/', include('api.commerce.checkout.urls')),
+    path('order_item/', include('api.commerce.order_item.urls')),
+]
