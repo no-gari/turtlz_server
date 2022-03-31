a5d413ee4f7589bfdc128a6dc3ada9c5fd18f476
no-gari <sofaissofa@icloud.com>
2022-03-31 13:06:43 +0900

    revised cart views

--- api/commerce/cart/admin.py
+++ api/commerce/cart/admin.py
@@ -4,11 +4,9 @@ from api.commerce.cart.models import Cart, CartItem
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
-    fields = ['product', 'quantity']
-    readonly_fields = ['product', 'quantity']
+    pass
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    fields = ['user', 'cart_item']
-    readonly_fields = ['user', 'cart_item']
+    pass
--- api/commerce/cart/urls.py
+++ api/commerce/cart/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
 from api.commerce.cart.views import CartRetrieveUpdateDestroyView, CreateCartItemView
+from django.urls import path
 
 
 urlpatterns = [
-    path('', CartRetrieveUpdateDestroyView),
-    path('add/', CreateCartItemView),
+    path('', CartRetrieveUpdateDestroyView.as_view()),
+    path('add/', CreateCartItemView.as_view()),
 ]
